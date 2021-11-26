e3d7bcac00557d7f49fdeec7f25714177954617f
st3fan7 <stefan.aradjanin@gmail.com>
2021-11-27 00:28:27 +0100

    :tada: Initialized webshop backend

---
+++ projects/web-shop-backend/src/main/java/com/example/webshopbackend/WebShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopBackendApplication.class, args);
+	}
+
+}
---
+++ projects/web-shop-backend/src/test/java/com/example/webshopbackend/WebShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
