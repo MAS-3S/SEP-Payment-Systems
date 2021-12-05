50a98b72521bc331d27b8653e2ebdc6a210cefe1
Stefan Beljic <stefan.beljic23@gmail.com>
2021-12-05 13:10:47 +0100

    :construction: Initialized pcc project and database configured

---
+++ projects/pcc-backend/src/main/java/pcc/pccbackend/PccBackendApplication.java
@@ -0,0 +1,13 @@
+package pcc.pccbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccBackendApplication.class, args);
+	}
+
+}
---
+++ projects/pcc-backend/src/main/java/pcc/pccbackend/model/Transaction.java
@@ -0,0 +1,128 @@
+package pcc.pccbackend.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "acquirer_order_id", nullable = false)
+    private String acquirerOrderId;
+
+    @Column(name = "acquirer_timestamp", nullable = false)
+    private LocalDateTime acquirerTimestamp;
+
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+
+    @Column(name = "pan", nullable = false)
+    private String pan;
+
+    @Column(name = "ccv", nullable = false)
+    private String ccv;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(name = "cardholder_name", nullable = false)
+    private String cardholderName;
+
+    @Column(name = "success", nullable = false)
+    private boolean success;
+
+    @Column(name = "message", nullable = false)
+    private String message;
+
+    public Transaction() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAcquirerOrderId() {
+        return acquirerOrderId;
+    }
+
+    public void setAcquirerOrderId(String acquirerOrderId) {
+        this.acquirerOrderId = acquirerOrderId;
+    }
+
+    public LocalDateTime getAcquirerTimestamp() {
+        return acquirerTimestamp;
+    }
+
+    public void setAcquirerTimestamp(LocalDateTime acquirerTimestamp) {
+        this.acquirerTimestamp = acquirerTimestamp;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+
+    public String getCcv() {
+        return ccv;
+    }
+
+    public void setCcv(String ccv) {
+        this.ccv = ccv;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCardholderName() {
+        return cardholderName;
+    }
+
+    public void setCardholderName(String cardholderName) {
+        this.cardholderName = cardholderName;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
