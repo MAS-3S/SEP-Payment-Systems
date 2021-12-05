c25a99eb5547a76e7cc31a8329d939371e1f7ed9
Stefan Beljic <stefan.beljic23@gmail.com>
2021-12-05 16:25:47 +0100

    :sparkles: Implemented methon for handle transaction

--- projects/issuer-bank-backend/src/main/java/issuer/bank/issuerbankbackend/service/ITransactionService.java
+++ projects/issuer-bank-backend/src/main/java/issuer/bank/issuerbankbackend/service/ITransactionService.java
@@ -1,4 +1,8 @@
 package issuer.bank.issuerbankbackend.service;
 
+import issuer.bank.issuerbankbackend.dto.PccRequest;
+import issuer.bank.issuerbankbackend.dto.PccResponse;
+
 public interface ITransactionService {
+    PccResponse handleTransactionRequest(PccRequest pccRequest);
 }
