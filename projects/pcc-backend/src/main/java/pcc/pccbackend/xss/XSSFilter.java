9690e307face1dfe3ef4b0344aa70831199ab209
st3fan7 <stefan.aradjanin@gmail.com>
2022-01-22 23:15:16 +0100

    :sparkles: Added XSS filter from ESAPI

---
+++ projects/pcc-backend/src/main/java/pcc/pccbackend/xss/XSSFilter.java
@@ -0,0 +1,38 @@
+package pcc.pccbackend.xss;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class XSSFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        XSSRequestWrapper wrappedRequest = new XSSRequestWrapper((HttpServletRequest) request);
+        String body = IOUtils.toString(wrappedRequest.getReader());
+        if (!StringUtils.isBlank(body)) {
+            body = XSSUtils.stripXSS(body);
+            wrappedRequest.resetInputStream(body.getBytes());
+        }
+        chain.doFilter(wrappedRequest, response);
+    }
+
+}
---
+++ projects/psp-service/src/main/java/com/example/pspservice/xss/XSSRequestWrapper.java
@@ -0,0 +1,119 @@
+package com.example.pspservice.xss;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+
+public class XSSRequestWrapper extends HttpServletRequestWrapper {
+
+    private byte[] rawData;
+    private final HttpServletRequest request;
+    private final ResettableServletInputStream servletStream;
+
+
+    public XSSRequestWrapper(HttpServletRequest request) {
+        super(request);
+        this.request = request;
+        this.servletStream = new ResettableServletInputStream();
+    }
+
+    public void resetInputStream(byte[] newRawData) {
+        rawData = newRawData;
+        servletStream.stream = new ByteArrayInputStream(newRawData);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if (rawData == null) {
+            rawData = IOUtils.toByteArray(this.request.getReader(), Charsets.UTF_8);
+            servletStream.stream = new ByteArrayInputStream(rawData);
+        }
+        return servletStream;
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        if (rawData == null) {
+            rawData = IOUtils.toByteArray(this.request.getReader(), Charsets.UTF_8);
+            servletStream.stream = new ByteArrayInputStream(rawData);
+        }
+        return new BufferedReader(new InputStreamReader(servletStream));
+    }
+
+
+
+    private static class ResettableServletInputStream extends ServletInputStream {
+
+        private InputStream stream;
+
+        @Override
+        public int read() throws IOException {
+            return stream.read();
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            return false;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+        }
+    }
+
+    @Override
+    public String[] getParameterValues(String parameter) {
+        String[] values = super.getParameterValues(parameter);
+        if (values == null) {
+            return null;
+        }
+        int count = values.length;
+        String[] encodedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            encodedValues[i] = XSSUtils.stripXSS(values[i]);
+        }
+        return encodedValues;
+    }
+
+    @Override
+    public String getParameter(String parameter) {
+        String value = super.getParameter(parameter);
+        return XSSUtils.stripXSS(value);
+    }
+
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        return XSSUtils.stripXSS(value);
+    }
+
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        List<String> result = new ArrayList<>();
+        Enumeration<String> headers = super.getHeaders(name);
+        while (headers.hasMoreElements()) {
+            String header = headers.nextElement();
+            String[] tokens = header.split(",");
+            for (String token : tokens) {
+                result.add(XSSUtils.stripXSS(token));
+            }
+        }
+        return Collections.enumeration(result);
+    }
+
+}
